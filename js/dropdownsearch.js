54547a40b170204522721e47d16ca30ed54c6af2
santoro12@gmail.com <santoro12@gmail.com@64baec4c-7543-1f2f-2983-4630b2b13f82>
2012-07-25 18:04:10 +0000

    Updated to re-bind after ajax queries run

--- js/dropdownsearch.js
+++ js/dropdownsearch.js
@@ -13,54 +13,71 @@ $.getScript('//dropdown-search.googlecode.com/svn/js/jquery.autocomplete.js', fu
       selectedIndex = selectedIndex != -1 ? selectedIndex : 0,
       qfTextField = "qfTextField" + selectName,
       $qfTextField;
-   console.log(selectedIndex);
   if($obj.children().length > 13 && !$obj.attr("multiple")){
-   $obj.hide();
-   $('<span><input name="' + qfTextField + '" id="' + qfTextField + '" type="text" value="' + searchTerms[selectedIndex] + '" /><img src="http://dropdown-search.googlecode.com/svn/images/arrow_down_left_sm.png" /></span>').insertAfter(this);
+   $obj
+    .addClass("ac_select")
+    .hide();
+   $('<span class="ac_span"><input class="ac_input" name="' + qfTextField + '" id="' + qfTextField + '" type="text" value="' + searchTerms[selectedIndex] + '" data-searchterms="' + searchTerms + '" /><img src="http://dropdown-search.googlecode.com/svn/images/arrow_down_left_sm.png" /></span>').insertAfter(this);
    $qfTextField = $("#" + qfTextField);
    $qfTextField
-   .on("click", function(){
-    $(this)
-     .data("prevValue", $(this).val() || $(this).data("prevValue"))
-     .val("");
-   })
-   .focusout(function(){
-    $(this).val($(this).val() || $(this).data("prevValue") || "");
-   })
    .next("img")
     .css({height: $obj.outerHeight(),
           position: "relative",
           "vertical-align": "middle"})
-    .bind("click", function(){
-     $qfTextField
-      .data("prevValue", $qfTextField.val() || $qfTextField.data("prevValue"))
-      .focus()
-      .val('')
-      .click();
-     setTimeout(function(){
-      $qfTextField.click();
-     }, 100);
-    })
    .end()
    .parent()
     .css({"white-space": "nowrap"})
    .end()
    .css({width: $obj.width() - $qfTextField.next("img").height(),
-         height: $obj.height()})
-    .querycomplete(searchTerms,
-                   {matchCase: false,
-                    matchContains: true,
-                    width: $obj.width() + 2,
-                    max: 10000,
-                    selectFirst: false,
-                    scroll: true,
-                    minChars: 0})
-    .result(function(event, data, formatted){
+         height: $obj.height()});
+  }
+ });
+ acBindings();
+ $("body").ajaxComplete(function(){
+   acBindings();
+ });
+});
+function acBindings(){
+ $('input.ac_input').each(function(e){
+   var $obj = $(this),
+       $selObj = $obj.parent().prev("select.ac_select:first"),
+       isBound = $obj.data("events") || 0;
+  if(!isBound){
+   $obj
+    .on("click", function(){
+     $(this)
+      .data("prevValue", $(this).val() || $(this).data("prevValue"))
+      .val("");
+    })
+    .focusout(function(){
+     $(this).val($(this).val() || $(this).data("prevValue") || "");
+    })
+    .next("img")
+     .bind("click", function(){
+      $obj
+       .data("prevValue", $obj.val() || $obj.data("prevValue"))
+       .focus()
+       .val('')
+       .click();
+      setTimeout(function(){
+       $obj.click();
+      }, 100);
+     })
+   .end()
+   .querycomplete($obj.data("searchterms").split(","),
+                  {matchCase: false,
+                   matchContains: true,
+                   width: $selObj.width() + 2,
+                   max: 10000,
+                   selectFirst: false,
+                   scroll: true,
+                   minChars: 0})
+   .result(function(event, data, formatted){
     if(data){
-     $obj.val($obj.find("option:contains(" + data + ")").val());
-     $obj.trigger("change");
+     $selObj.val($selObj.find("option:contains(" + data + ")").val());
+     $selObj.trigger("change");
     }
    });
   }
  });
-});
+}
