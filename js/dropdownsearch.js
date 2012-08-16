5ea34c72f3bcd3da44c5df390be71c2a44b2bb67
santoro12@gmail.com <santoro12@gmail.com@64baec4c-7543-1f2f-2983-4630b2b13f82>
2012-08-16 17:40:38 +0000

    Added manual remove and add options.

--- js/dropdownsearch.js
+++ js/dropdownsearch.js
@@ -4,39 +4,91 @@ javascript:void(function(){
  document.getElementsByTagName('head')[0].appendChild(s);
 }());
 $.getScript('//dropdown-search.googlecode.com/svn/js/jquery.autocomplete.js', function(){
+ var actMenus = [];
  $("head").append('<link rel="stylesheet" type="text/css" href="http://dropdown-search.googlecode.com/svn/styles/dropdown.css">');
- $('select:visible').each(function(e){
-  var $obj = $(this),
-      selectName = $obj.attr("name"),
-      searchTerms = $obj.children().map(function() {return $(this).html();}).get(),
-      selectedIndex = $('option[selected="selected"]', this).index(),
-      selectedIndex = selectedIndex != -1 ? selectedIndex : 0,
-      qfTextField = "qfTextField" + selectName,
-      $qfTextField;
-  if($obj.children().length > 13 && !$obj.attr("multiple")){
-   $obj
-    .addClass("ac_select")
-    .hide();
-   $('<span class="ac_span"><input class="ac_input" name="' + qfTextField + '" id="' + qfTextField + '" type="text" value="' + searchTerms[selectedIndex] + '" data-searchterms="' + searchTerms + '" /><img src="http://dropdown-search.googlecode.com/svn/images/arrow_down_left_sm.png" /></span>').insertAfter(this);
-   $qfTextField = $("#" + qfTextField);
-   $qfTextField
-   .next("img")
-    .css({height: $obj.outerHeight(),
-          position: "relative",
-          "vertical-align": "middle"})
-   .end()
-   .parent()
-    .css({"white-space": "nowrap"})
-   .end()
-   .css({width: $obj.width() - $qfTextField.next("img").height(),
-         height: $obj.height()});
-  }
+ $('select:visible:not(#pageNameDD)').each(function(event){
+  buildQArea($(this), this);
  });
  acBindings();
  $("body").ajaxComplete(function(){
    acBindings();
  });
+ actMenus.push('<div id="actSearchMenu">');
+ actMenus.push('Activate Search');
+ actMenus.push('</div>');
+ actMenus.push('<div id="deactSearchMenu">');
+ actMenus.push('Deactivate Search');
+ actMenus.push('</div>');
+ $('body')
+  .append(actMenus.join(""));
+ $('#actSearchMenu, #deactSearchMenu')
+  .css({
+   display: 'none',
+   position: 'absolute',
+   padding: '5px',
+   background: '#000',
+   border: '1px solid #fff',
+   color: '#fff',
+   cursor: 'pointer'
+  });
+ $("select").bind("contextmenu", function(event){
+  $('#actSearchMenu')
+   .css({
+    top: event.pageY + 'px',
+    left: event.pageX + 'px'
+   })
+   .data('dropdown', $(this))
+   .data('refr', this)
+   .show();
+  return false;
+ });
+ $('#actSearchMenu').click(function(){
+  buildQArea($(this).data('dropdown'), $(this).data('refr'));
+  acBindings();
+  $('#actSearchMenu').hide();
+ });
+ $('#deactSearchMenu').click(function(){
+  $obj = $(this).data('refr');
+  $obj
+   .parent()
+    .prev('.ac_select')
+    .show()
+   .end()
+   .remove();
+  $('#deactSearchMenu').hide();
+ });
+ $(document).click(function(){
+  $('#actSearchMenu, #deactSearchMenu').hide();
+ });
 });
+function buildQArea(obj, self){
+ var $obj = obj,
+     selectName = $obj.attr("name"),
+     searchTerms = $obj.children().map(function() {return $(this).html();}).get(),
+     selectedIndex = $('option[selected="selected"]', self).index(),
+     selectedIndex = selectedIndex != -1 ? selectedIndex : 0,
+     qfTextField = "qfTextField" + selectName,
+     $qfTextField;
+  //console.log(selectedIndex);
+ if($obj.children().length > 13 && !$obj.attr("multiple")){
+  $obj
+   .addClass("ac_select")
+   .hide();
+  $('<span class="ac_span"><input class="ac_input" name="' + qfTextField + '" id="' + qfTextField + '" type="text" value="' + searchTerms[selectedIndex] + '" data-searchterms="' + searchTerms + '" /><img src="http://dropdown-search.googlecode.com/svn/images/arrow_down_left_sm.png" /></span>').insertAfter(self);
+  $qfTextField = $("#" + qfTextField);
+  $qfTextField
+  .next("img")
+   .css({height: $obj.outerHeight(),
+         position: "relative",
+         "vertical-align": "middle"})
+  .end()
+  .parent()
+   .css({"white-space": "nowrap"})
+  .end()
+  .css({width: $obj.width() - $qfTextField.next("img").height(),
+        height: $obj.height()});
+ }
+}
 function acBindings(){
  $('input.ac_input').each(function(e){
    var $obj = $(this),
@@ -53,15 +105,27 @@ function acBindings(){
      $(this).val($(this).val() || $(this).data("prevValue") || "");
     })
     .next("img")
-     .bind("click", function(){
-      $obj
-       .data("prevValue", $obj.val() || $obj.data("prevValue"))
-       .focus()
-       .val('')
-       .click();
-      setTimeout(function(){
-       $obj.click();
-      }, 100);
+    .bind({
+      click: function(event){
+       $obj
+        .data("prevValue", $obj.val() || $obj.data("prevValue"))
+        .focus()
+        .val('')
+        .click();
+       setTimeout(function(){
+        $obj.click();
+       }, 100);
+      },
+      contextmenu: function(event){
+       $('#deactSearchMenu')
+        .css({
+         top: event.pageY+'px',
+         left: event.pageX+'px'
+        })
+        .data('refr', $(this))
+        .show();
+        return false;
+      }
      })
    .end()
    .querycomplete($obj.data("searchterms").split(","),
